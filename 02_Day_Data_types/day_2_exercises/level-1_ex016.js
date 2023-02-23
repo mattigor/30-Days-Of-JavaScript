8b4d1d1534a92a1ed4b3a0d5682d835749bef1c0
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:40:29 -0300

    Update level-1_ex016.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex016.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex016.js
@@ -1,3 +1,5 @@
+// Use lastIndexOf to determine the position of the last occurrense of a in 30 Days Of JavaScript
+
 const challenge = "30 Days Of JavaScript";
 
 console.log(challenge.lastIndexOf("a"));
