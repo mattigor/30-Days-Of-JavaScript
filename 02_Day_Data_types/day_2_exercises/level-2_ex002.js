8a3bb210f38d864a1f9b3463701e513370c28422
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:11:10 -0300

    Update level-2_ex002.js

--- 02_Day_Data_types/day_2_exercises/level-2_ex002.js
+++ 02_Day_Data_types/day_2_exercises/level-2_ex002.js
@@ -1,3 +1,5 @@
+// 2. Use console.log() to print out the following quote by Mother Teresa: Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead
+
 const quote = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
 
 console.log(quote);
