f9aba13f6cf9041d7fc8a66f8c851239916db470
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 10:30:07 -0300

    Update level-3_ex001.js

--- 02_Day_Data_types/day_2_exercises/level-3_ex001.js
+++ 02_Day_Data_types/day_2_exercises/level-3_ex001.js
@@ -3,4 +3,4 @@
 const sentence = "Love is the best thing in this world. Some found their love and some are still looking for their love";
 const found = sentence.match(/love/g).length + sentence.match(/Love/g).length;
 
-console.log(`Number of love's found: ${found}`);
+console.log(`Number of love's found: ${found}.`);
