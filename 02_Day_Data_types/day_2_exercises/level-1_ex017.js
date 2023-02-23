23cd89d7b4a28af0b07ed9fd77b1d2a55e07cff3
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:41:48 -0300

    Update level-1_ex017.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex017.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex017.js
@@ -1,3 +1,5 @@
+// 17. Use indexOf to find the position of the first occurrense of the word because in the follow sentence: 'You cannot end a sentence with because because because is a conjunction'
+
 const sentence = "You cannot end a sentence with because because because is a conjunction";
 
 console.log(sentence.indexOf("because"));
