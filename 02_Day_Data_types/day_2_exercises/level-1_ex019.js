6daf3909c455270dc010fde4c7f5306e3327360e
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:00:51 -0300

    Update level-1_ex019.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex019.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex019.js
@@ -1,3 +1,5 @@
+// 19. Use search to find the position of the first occurrense of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
 const sentence = "You cannot end a sentence with because because because is a conjunction";
 
 console.log(sentence.search("because"));
