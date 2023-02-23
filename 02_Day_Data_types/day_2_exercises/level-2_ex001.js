463f1b34087d5082a4467cbb8cf09b6b06f371cf
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:10:16 -0300

    Update level-2_ex001.js

--- 02_Day_Data_types/day_2_exercises/level-2_ex001.js
+++ 02_Day_Data_types/day_2_exercises/level-2_ex001.js
@@ -1,3 +1,5 @@
+// 1. Use console.log() to print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up'. by John Holmes teaches us to help one another
+
 const statement = "The quote 'There is no exercise better for the heart than reaching down and lifting people up'. by John Holmes teaches us to help one another.";
 
 console.log(statement);
