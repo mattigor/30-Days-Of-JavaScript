ffcfb8f08ca026de283e8af0b7acd7e2531d91d1
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:17:31 -0300

    Update ex009.js

--- 01_Day_Introduction/day_1_exercises/ex009.js
+++ 01_Day_Introduction/day_1_exercises/ex009.js
@@ -1,1 +1,3 @@
+// 9. Declare variables to store your first name, last name, marital status, country and age in single line
+
 let firstName = "Igor Matheus", lastName = "Palma da Rosa", maritalStatus = "Single, but not single", country = "Brazil", age = 24;
