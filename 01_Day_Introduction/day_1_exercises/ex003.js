53d76d6cfe2830dc9ea2a2f0e1b6e4279e5e65b9
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:12:58 -0300

    Update ex003.js

--- 01_Day_Introduction/day_1_exercises/ex003.js
+++ 01_Day_Introduction/day_1_exercises/ex003.js
@@ -1,4 +1,4 @@
-// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
 
 /*
     comments can make code readable, easy to reuse and informative
