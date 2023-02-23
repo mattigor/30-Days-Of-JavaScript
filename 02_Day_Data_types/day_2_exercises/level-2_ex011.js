3f29e597de626e73f771101edf2dd652f13b259a
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:19:23 -0300

    Update level-2_ex011.js

--- 02_Day_Data_types/day_2_exercises/level-2_ex011.js
+++ 02_Day_Data_types/day_2_exercises/level-2_ex011.js
@@ -1,5 +1,5 @@
 /*
-    Use console.log() and escape characters to print the following pattern
+    11. Use console.log() and escape characters to print the following pattern
     1 1 1 1 1
     2 1 2 4 8
     3 1 3 9 27
