d8728f702c598ab79f09ca41e811451f456a8af9
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:02:00 -0300

    Update level-1_ex020.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex020.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex020.js
@@ -1,3 +1,5 @@
+// 20. Use trim() method to remove and trailing whitespaces at the beggining and the end of a string.E.g ' 30 Days Of JavaScript '
+
 const challenge = " 30 Days Of JavaScript ";
 
 console.log(challenge.trim(0, 22));
