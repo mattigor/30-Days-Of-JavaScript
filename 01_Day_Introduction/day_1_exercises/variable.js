3122806299076214b04e2664e211b202dc099e19
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:13:08 -0300

    Update variable.js

--- 01_Day_Introduction/day_1_exercises/variable.js
+++ 01_Day_Introduction/day_1_exercises/variable.js
@@ -1,4 +1,4 @@
-// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
 let str = "String";
 let bol = true;
