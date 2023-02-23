4ee8b1c477bf4d188705be7596ef05fea16563f4
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:59:44 -0300

    Update level-1_ex018.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex018.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex018.js
@@ -1,3 +1,5 @@
+// 18. Use lastIndexOf to find the position of the last occurrense of the word because in the follow sentence: 'You cannot end a sentence with because because because is a conjunction'
+
 const sentence = "You cannot end a sentence with because because because is a conjunction";
 
 console.log(sentence.lastIndexOf("because"));
