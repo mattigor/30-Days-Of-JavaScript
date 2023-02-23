cf0d7c561ea8a3e52cfce0bd20cdd1d068127c85
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:36:06 -0300

    Update level-1_ex011.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex011.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex011.js
@@ -1,3 +1,5 @@
+// 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array
+
 const bigTechCompanies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
 
 console.log(bigTechCompanies.split(","));
