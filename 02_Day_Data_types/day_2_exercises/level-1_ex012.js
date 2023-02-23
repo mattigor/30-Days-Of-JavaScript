1c6776a087d07a36c618e44d6410125bdb70ca9d
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:37:03 -0300

    Update level-1_ex012.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex012.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex012.js
@@ -1,3 +1,5 @@
+// 12. Change 30 Days Of JavaScript to 30 Days Of Python using replace() method
+
 const challenge = "30 Days Of JavaScript";
 
 console.log(challenge.replace("JavaScript", "Python"));
