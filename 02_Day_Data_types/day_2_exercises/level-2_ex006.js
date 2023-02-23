de3f230751ff1eba41ad801fa47f9c028f7442e5
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:14:52 -0300

    Update level-2_ex006.js

--- 02_Day_Data_types/day_2_exercises/level-2_ex006.js
+++ 02_Day_Data_types/day_2_exercises/level-2_ex006.js
@@ -1,3 +1,5 @@
+// 6. I hope this course is not full of jargon. Check if jargon is in the sentence
+
 const sentence = "I hope this course if not full of jargon";
 
 console.log(sentence.includes("jargon")? "yes" : "no");
