513489f39c07a24b11d244974b3dc5ccd2c9167e
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:14:12 -0300

    Update datatypes.js

--- 01_Day_Introduction/day_1_exercises/datatypes.js
+++ 01_Day_Introduction/day_1_exercises/datatypes.js
@@ -1,3 +1,5 @@
+// 5. Create a datatypes.js file and use the JavaScript tyof operator to check different data types. Check the data type of each variable
+
 let str = "String";
 let bol = true;
 let und;
