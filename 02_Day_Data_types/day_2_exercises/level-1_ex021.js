30e89e70a422c9116f8272b34218f58b17fb7e2a
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 09:02:44 -0300

    Update level-1_ex021.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex021.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex021.js
@@ -1,3 +1,5 @@
+// 21. Use startsWith() method with the string '30 Days Of JavaScript' and make the result true
+
 const challenge = "30 Days Of JavaScript";
 
 console.log(challenge.startsWith("3"));
