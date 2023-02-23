e01952a4e5cf98008f761bb667737b3e602c0fc3
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:39:40 -0300

    Update level-1_ex015.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex015.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex015.js
@@ -1,3 +1,5 @@
+// 15. Use indexOf to determine the position of the first occurrense of a in 30 Days Of JavaScript
+
 const challenge = "30 Days Of JavaScript";
 
 console.log(challenge.indexOf("a"));
