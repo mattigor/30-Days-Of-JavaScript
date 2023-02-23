df35f50a06215d8f3cf301553cb4361467bc312c
Igor Matheus <mattigor.impr@gmail.com>
2023-02-23 08:38:59 -0300

    Update level-1_ex014.js

--- 02_Day_Data_types/day_2_exercises/level-1_ex014.js
+++ 02_Day_Data_types/day_2_exercises/level-1_ex014.js
@@ -1,3 +1,5 @@
+// 14. What is the character code of J in '30 Days Of JavaScript' string? Use charCodeAt() method
+
 const challenge = "30 Days Of JavaScript";
 
 console.log(challenge.charCodeAt(11));
